54b80d9c7d31649bac68fd5b6e08f46298da6a3b
Debora Dian Pratama <deboradian09@gmail.com>
2018-11-01 14:48:04 +0700

    all done

--- src/main/java/com/apap/tutorial7/rest/Setting.java
+++ src/main/java/com/apap/tutorial7/rest/Setting.java
@@ -2,4 +2,5 @@ package com.apap.tutorial7.rest;
 
 public class Setting {
 	final public static String pilotUrl = "https://8f9de02a-8f4a-458e-a3ec-3fa57acd5f7d.mock.pstmn.io";
+	final public static String airportUrl = "https://api.sandbox.amadeus.com/v1.2/airports/autocomplete?apikey=Sv4EGPie7hUoeMfPcsHPVmYG3dayVesI&term=";
 }
